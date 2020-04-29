305d3d66962ff7db7597082153f14f3d9574126a
dikers <dikers@gmail.com>
2020-04-29 09:31:09 +0800

    添加对比代码

---
+++ recognize_process/shell/test-cn.sh
@@ -0,0 +1,7 @@
+DATA_TARGET_DIR='./data_cn/'
+export PYTHONPATH=../../
+python3 ../tools/test_crnn_jmz.py \
+-i=${DATA_TARGET_DIR}'images/train' \
+-w=${DATA_TARGET_DIR}'model_save' \
+-c=${DATA_TARGET_DIR}'char_map.json' \
+-t=${DATA_TARGET_DIR}'test_labels.txt'
